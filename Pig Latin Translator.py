07c06accc4fb8ebb9c5e34ed8839fdc45fc8805d
Devlin0120 <devlinrogue12@gmail.com>
2023-02-15 15:28:43 -0700

    Add files via upload

---
+++ Pig Latin Translator.py
@@ -0,0 +1,14 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    print original
+else:
+    print 'empty'
+new_word = word + first + pyg
+new_word = original[1:len(new_word)] + first + pyg
+print 'translates to:'
+print new_word
